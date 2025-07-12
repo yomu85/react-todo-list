d8179ac352e9f27835d8ca5a427eccfa4621fdec
jacob <yomu85@gmail.com>
2025-07-12 19:22:28 +0900

    #3 할 일 추가 기능 개선

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,24 @@ import './App.css'
 
 function App() {
   const [items, setItems] = useState<string[]>([]);
+  const [inputValue, setInputValue] = useState("");
   const inputRef = useRef<HTMLInputElement>(null);
 
   const handlerAddItem = () => {
-    const inputValue = inputRef.current?.value.trim();
-    if (!inputValue) return;
-    setItems((prevItems) => [...prevItems, inputValue]);
-    inputRef.current!.value = ''; // Clear the input field after adding the item
+    if(!inputValue.trim()) { // 입력값이 비어있거나 공백만 있는 경우 추가하지 않음
+      inputRef.current?.focus(); // 입력 후 입력창에 포커스 유지
+      return;
+    }
+    setItems(prev => [...prev, inputValue.trim()]);
+    setInputValue(""); // 입력 후 입력창 비우기
+    inputRef.current?.focus(); // 입력 후 입력창에 포커스 유지
   }
+
+  
   return (
     <section className="todo-app">
       <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
-        <Input ref={inputRef} placeholder="할 일을 입력하세요"  />
+        <Input ref={inputRef} value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder="할 일을 입력하세요"  />
         <Button onClick={handlerAddItem}>
           저장
           <Save />
